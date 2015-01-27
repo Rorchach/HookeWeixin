7b6539061872f89a683d54038240ccf6d867a25b
Rorchach <cookmycode@gmail.com>
2015-01-27 13:46:54 +0800

    mod config

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var request = require('request');
 
-var TOKEN = 'ACCESS_TOKEN';
+var APPID = 'wx35f69d06fa9315f8';
+var SERECT = '9fa6b3e7b45f5ffe14a3f970aaa36201';
+var TOKEN_URL = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + APPID + '&secret=' + SERECT;
+
+var access_token, expires_in
+
+setInterval(function(){
+    request(TOKEN_URL, function(error, response, body){
+        body = JSON.parse(body);
+
+        access_token = body.access_token;
+        expires_in = body.expires_in;
+        console.log(access_token, expires_in);
+    });
+},  60 * 1000);
+
 
 app.get('/', function(req, res){
     res.send(req.param('signatrue'));
